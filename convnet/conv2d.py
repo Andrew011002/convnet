88ee9626e9a0c1da1b2580d286de15c1c91f4e85
andmholm <andmholm@iu.edu>
2023-11-15 16:53:03 -0500

    finished conv2d, working on conv3d

--- convnet/conv2d.py
+++ convnet/conv2d.py
@@ -1,53 +1,73 @@
 import numpy as np
 
 
-def conv2d(image, kernel, stride=1, padding="valid", fill=None):
-    image = handle_padding(image, kernel.shape[0], stride, padding, fill)
-    h, w = image.shape
-    k = kernel.shape[0]
-    out = np.zeros(((h - k) // stride + 1, (w - k) // stride + 1))
-    for i in range(0, h - k + 1, stride):
-        for j in range(0, w - k + 1, stride):
-            out[i // stride, j // stride] = \
-                np.sum(image[i:i + k, j:j + k] * kernel)
-    return out
+class Conv2D:
 
+    def __init__(self, kernel_size, stride=1, padding="valid", fill=0):
+        assert len(kernel_size) == 2, "can only convolve with 2D kernel"
+        assert kernel_size[0] == kernel_size[1], "can only convolve with square kernels"
+        assert stride >= 1, "can't implement stride smaller than 1"
+        if type(padding) in [int, float]:
+            padding = (padding, padding)
+        if isinstance(padding, tuple):
+            assert len(padding) == 2, "can only pad with tuple of 2 values"
+            assert all(p in [int, float] for p in padding), \
+                "can only pad with numeric types"
+        else:
+            assert padding in ["valid", "same"], \
+                "padding types can only be valid or same"
+        assert type(fill) in [int, float], "can only fill with numeric types"
 
-def handle_padding(image, k, stride, padding, fill):
-    # assumes square image
-    if padding == "same":
-        p = (k - image.shape[0] * (1 - stride) - stride) // 2
-    elif padding == "valid":
-        p = 0
-    elif isinstance(padding, int):
-        p = padding
-    fill = 0 if fill is None else fill
-    image = pad(image, p, fill)
-    return image
+        self.kernel_size = kernel_size
+        self.stride = stride
+        self.padding = padding
+        self.fill = fill
+        self.weight = np.random.randn(*kernel_size)
+        self.bias = np.zeros(1)
 
+    def forward(self, x):
+        x = self.pad(x, self.weight, self.stride, self.padding, self.fill)
+        return self.convovle2d(x, self.weight, self.stride) + self.bias
 
-def pad(image, p=1, fill=0):
-    if p == 0:
-        return image
-    h, w = image.shape
-    n, m = h + 2 * p, w + 2 * p
-    padded = np.full((n, m), fill_value=float(fill))
-    padded[p:h + p, p:w + p] = image
-    return padded
+    def convovle2d(self, input, kernel, stride):
+        b, h, w = input.shape
+        k = kernel.shape[0]
+        shape = (b, (h - k) // stride + 1, (w - k) // stride + 1)
+        output = np.empty(shape)
+        for i in range(0, h - k + 1, stride):
+            for j in range(0, w - k + 1, stride):
+                out = np.sum(input[:, i: i + k, j: j + k]
+                             * kernel, axis=(1, 2))
+                output[:, i // stride, j // stride] = out
+        return output
+
+    def pad(self, input, kernel, stride=1, padding="valid", fill=0):
+        b, h, w = input.shape
+        k, _ = kernel.shape
+        pad = self._get_pad_val(h, w, k, stride, padding)
+        n, m = h + 2 * pad[0], w + 2 * pad[1]
+        pad_arr = np.full((b, n, m), fill_value=fill, dtype=np.float32)
+        pad_arr[:, pad[0]:pad[0] + h, pad[1]:pad[1] + w] = input
+        return pad_arr
+
+    def _get_pad_val(self, h, w, k, stride, padding):
+        if padding == "valid":
+            return (0, 0)
+        elif padding == "same":
+            p_h = (k + h * (1 - stride) - stride) // 2
+            p_w = (k + w * (1 - stride) - stride) // 2
+            return (p_h, p_w)
+        return padding
 
 
 def main():
-    image = np.random.randn(5, 5)
-    kernel = np.zeros((3, 3))
-    kernel[1, 1] = 1
-    convd = conv2d(image, kernel, stride=1, padding="valid")
-    assert image[1, 1] == convd[0, 0]
-
-    convd = conv2d(image, kernel, stride=1, padding="same", fill=0)
-    assert np.array_equal(image, convd)
-
-    convd = conv2d(image, kernel, stride=2, padding="valid")
-    assert image[1, 1] == convd[0, 0]
+    input = np.random.randn(16, 32, 32)
+    conv2d = Conv2D((3, 3), stride=1, padding="same")
+    conv2d.weight = np.zeros_like(conv2d.weight)
+    conv2d.weight[1, 1] = 1.0
+    output = conv2d.forward(input)
+    print(output.shape)
+    assert np.allclose(input, output)
 
 
 if __name__ == "__main__":
--- convnet/conv3d.py
+++ convnet/conv3d.py
@@ -1,76 +1,77 @@
 import numpy as np
 
 
-def conv3d(image, kernel, stride=1, padding="same", fill=None):
-    image = handle_padding(image, kernel.shape[0], stride, padding, fill)
-    h, w, c = image.shape
-    k = kernel.shape[0]
+class Conv3D:
 
-    out = np.zeros(((h - k) // stride + 1, (w - k) // stride + 1))
-    for i in range(0, h - k + 1, stride):
-        for j in range(0, w - k + 1, stride):
-            out[i // stride, j // stride] = \
-                np.sum(image[i:i + k, j:j + k, :] * kernel)
-    return out
+    def __init__(self, in_channels, out_channels, kernel_size, stride=1, padding="valid", fill=0):
+        assert len(kernel_size) == 2, "can only convolve with 2D kernel"
+        assert kernel_size[0] == kernel_size[1], "can only convolve with square kernels"
+        assert stride >= 1, "can't implement stride smaller than 1"
+        if type(padding) in [int, float]:
+            padding = (padding, padding)
+        if isinstance(padding, tuple):
+            assert len(padding) == 2, "can only pad with tuple of 2 values"
+            assert all(p in [int, float] for p in padding), \
+                "can only pad with numeric types"
+        else:
+            assert padding in ["valid", "same"], \
+                "padding types can only be valid or same"
+        assert type(fill) in [int, float], "can only fill with numeric types"
 
+        self.kernel_size = kernel_size
+        self.stride = stride
+        self.padding = padding
+        self.fill = fill
+        self.weight = np.random.randn(
+            kernel_size[0], kernel_size[1], in_channels, out_channels)
+        self.bias = np.zeros((1, 1, out_channels))
 
-def handle_padding(image, k, stride, padding, fill):
-    # assumes square image
-    if padding == "same":
-        p = (k - image.shape[0] * (1 - stride) - stride) // 2
-    elif padding == "valid":
-        p = 0
-    elif isinstance(padding, int):
-        p = padding
-    fill = 0 if fill is None else fill
-    image = pad(image, p, fill)
-    return image
+    def forward(self, x):
+        x = self.pad(x, self.weight[:, :, :, 0],
+                     self.stride, self.padding, self.fill)
+        return self.convovle3d(x, self.weight, self.stride) + self.bias
 
+    def convovle3d(self, input, kernel, stride):
+        b, h, w, c = input.shape
+        k, _, c, n_kernels = kernel.shape
+        shape = (b, (h - k) // stride + 1, (w - k) //
+                 stride + 1, n_kernels)
+        output = np.empty(shape)
+        for out_c in range(n_kernels):
+            for i in range(0, h - k + 1, stride):
+                for j in range(0, w - k + 1, stride):
+                    out = np.sum(input[:, i: i + k, j: j + k]
+                                 * self.weight[:, :, :, out_c], axis=(1, 2, 3))
+                    output[:, i // stride, j // stride, out_c] = out
+        return output
 
-def pad(image, p=1, fill=0):
-    if p == 0:
-        return image
-    h, w, c = image.shape
-    n, m = h + 2 * p, w + 2 * p
-    padded = np.full((n, m, c), fill_value=float(fill))
-    padded[p:h + p, p:w + p, :] = image
-    return padded
+    def pad(self, input, kernel, stride=1, padding="valid", fill=0):
+        b, h, w, c = input.shape
+        k, _, c = kernel.shape
+        pad = self._get_pad_val(h, w, k, stride, padding)
+        n, m = h + 2 * pad[0], w + 2 * pad[1]
+        pad_arr = np.full((b, n, m, c), fill_value=fill, dtype=np.float32)
+        pad_arr[:, pad[0]:pad[0] + h, pad[1]:pad[1] + w, :] = input
+        return pad_arr
 
-
-def conv3d_multi(image, kernels, stride=1, padding="same", fill=None):
-    convds = []
-    for i in range(kernels.shape[0]):
-        kernel = kernels[i]
-        convd = conv3d(image, kernel, stride, padding, fill)
-        convds.append(convd)
-    return np.array(convds).transpose((1, 2, 0))
+    def _get_pad_val(self, h, w, k, stride, padding):
+        if padding == "valid":
+            return (0, 0)
+        elif padding == "same":
+            p_h = (k + h * (1 - stride) - stride) // 2
+            p_w = (k + w * (1 - stride) - stride) // 2
+            return (p_h, p_w)
+        return padding
 
 
 def main():
-    image = np.random.randn(5, 5, 3)
-    kernel = np.zeros((3, 3, 3))
-    kernel[1, 1, :] = 1
-    convd = conv3d(image, kernel, stride=1, padding="valid")
-    assert (convd[0, 0] == np.sum(image[1, 1, :]))
-
-    convd = conv3d(image, kernel, stride=1, padding="same", fill=0)
-    assert np.array_equal(image.sum(axis=2), convd)
-
-    convd = conv3d(image, kernel, stride=2, padding="valid")
-    assert image[1, 1].sum() == convd[0, 0]
-
-    n_k = 64
-    kernels = np.random.rand(n_k, 3, 3, 3)
-    convd = conv3d_multi(image, kernels, stride=1, padding="valid")
-    assert convd.shape == (3, 3, 64)
-
-    kernels = np.random.rand(n_k, 3, 3, 3)
-    convd = conv3d_multi(image, kernels, stride=1, padding="same", fill=0)
-    assert convd.shape == (5, 5, 64)
-
-    kernels = np.random.rand(n_k, 3, 3, 3)
-    convd = conv3d_multi(image, kernels, stride=2, padding="valid")
-    assert convd.shape == (2, 2, 64)
+    conv3d = Conv3D(3, 3, kernel_size=(3, 3), padding="same")
+    conv3d.weight = np.zeros_like(conv3d.weight)
+    conv3d.weight[1, 1, :, :] = 1
+    input = np.random.randn(32, 9, 9, 3)
+    output = conv3d.forward(input)
+    print(output.shape)
+    assert np.allclose(input, output)
 
 
 if __name__ == "__main__":
