94b9bf020f39a3baef1bfa0d0aaeb2a1e0c8ac8f
Andrew <andrewholmes011002@gmail.com>
2023-11-17 20:27:33 -0500

    added the avg pool layer as well

--- convnet/conv_layer.py
+++ convnet/conv_layer.py
@@ -1,6 +1,7 @@
 import numpy as np
 from conv2d import Conv2D
 from maxpool2d import MaxPool2D
+from avgpool2d import AvgPool2D
 
 
 def relu(z):
@@ -12,6 +13,16 @@ def main():
     input = np.random.rand(32, 3, 28, 28) - 0.5
     conv2d = Conv2D(3, 16, kernel_size=(5, 5), stride=1, padding="valid")
     maxpool2d = MaxPool2D(kernel_size=(2, 2), stride=2, padding="valid")
+    conv_output = conv2d.forward(input)
+    pool_output = maxpool2d.forward(conv_output)
+    activation = relu(pool_output)
+    assert activation.shape == (32, 16, 12, 12)
+    assert np.min(activation) == 0
+    assert np.max(activation) == np.max(pool_output)
+
+    input = np.random.rand(32, 3, 28, 28) - 0.5
+    conv2d = Conv2D(3, 16, kernel_size=(5, 5), stride=1, padding="valid")
+    maxpool2d = AvgPool2D(kernel_size=(2, 2), stride=2, padding="valid")
     conv_output = conv2d.forward(input)
     pool_output = maxpool2d.forward(conv_output)
     activation = relu(pool_output)
